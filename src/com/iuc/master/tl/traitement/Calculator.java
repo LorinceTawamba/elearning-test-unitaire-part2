1c989bf84d106f0f5f2a0cee239ccc39676cd679
LorinceTawamba <lorince.tawamba@gmail.com>
2022-04-26 19:25:54 +0100

    Update

--- src/com/iuc/master/tl/traitement/Calculator.java
+++ src/com/iuc/master/tl/traitement/Calculator.java
@@ -7,15 +7,23 @@ public class Calculator {
     }
 
     public double addition(double op1, double op2) {
-
+        return op1 + op2;
     }
 
     public double soustraction(double op1, double op2) {
+        return op1 - op2;
     }
 
     public double multiplication(double op1, double op2) {
+        return op1 * op2;
     }
 
     public double division(double op1, double op2) {
+        if (op1 != 0) {
+            return op1 / op2;
+        }
+        else {
+            return Double.NaN;
+        }
     }
 }
